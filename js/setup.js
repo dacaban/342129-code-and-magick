75ef1b7b9297096f956c8bc816252a8471b3116c
Daria <dasha_net96@mail.ru>
2018-11-21 20:50:04 +0300

    нашла и отобразила блок с похожими персонажами. Нашла блок-шаблон с персонажем.

--- js/setup.js
+++ js/setup.js
@@ -2,3 +2,7 @@
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
