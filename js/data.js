4b2cfaa93e8cbe24f3b8ebb1dd452c39b51e3072
Daria <dasha_net96@mail.ru>
2018-12-05 22:02:56 +0300

    разбивает код на модули

---
+++ js/data.js
@@ -0,0 +1,14 @@
+'use strict';
+(function () {
+  window.data = {
+    WIZARD_NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+    WIZARD_LASTNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+    WIZARD_COATS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+    WIZARD_EYES: ['black', 'red', 'blue', 'yellow', 'green'],
+    WIZARD_FIREBALLS: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'],
+    SIMILAR_WIZARDS: 4,
+    ESC_KEYCODE: 27,
+    ENTER_KEYCODE: 13,
+    userDialog: document.querySelector('.setup')
+  };
+})();
